c88c31068460c084d58ebfad17e27926f1971570
Surasak La-ongkham <surasak.ict.msu@gmail.com>
2017-03-10 07:53:36 +0700

    change uri mongo2

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {  
 	'secret': 'adfeadf524dfe5adfe8@df#d',  
-	'database': process.env.MONGODB_URI || 'mongodb://localhost/itds-vec'  
+	'database': process.env.MONGOLAB_URI || 'mongodb://localhost/itds-vec'  
 };
